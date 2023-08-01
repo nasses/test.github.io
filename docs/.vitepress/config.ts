5679440105a8c1f2d859d203fd37511e11b1321d
nasses <464166566@qq.com>
2023-08-01 15:07:45 +0800

    11111

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base："/test.github.io/",
   title: "my project",
   description: "A VitePress Site",
   themeConfig: {
